bfba8b8f30076506b991dcb8b07b6357465e9b26
Diego Steiner <diego.steiner@renuo.ch>
2012-02-16 21:54:46 +0100

    moved abteilung

--- app/controllers/organisational_units_controller.rb
+++ app/controllers/organisational_units_controller.rb
@@ -1,5 +1,9 @@
 class OrganisationalUnitsController < ApplicationController
   #load_and_authorize_resource
+  
+  def index
+    redirect_to organisational_unit_path('abteilung')
+  end
 
   def show
     @ou = OrganisationalUnit.find_by_name(params[:id])
