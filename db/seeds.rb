f43fa5eaba0663d6025082aaf561e55e7d0e679c
Andre Bürkler <gulli@gulli-laptop.(none)>
2012-04-12 21:17:10 +0200

    text

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ ou[:elternrat] = OrganisationalUnit.create!(name: 'Elternrat', caption: 'Die Elt
 
 
 filou = Member.create!(first_name: 'Diego', last_name: 'Steiner', scout_name: 'Filou', email: 'filou@pfadismn.ch', birthdate: 23.years.ago, gender: 0, member_since: 14.years.ago, education: "Pano (2010)")
-gulli = Member.create!(first_name: 'André', last_name: 'Bürkler', scout_name: 'Gulli', email: 'gulli@pfadismn.ch', birthdate: 24.years.ago, gender: 0)
+gulli = Member.create!(first_name: 'André', last_name: 'Bürkler', scout_name: 'Gulli', email: 'gulli@pfadismn.ch', birthdate: 24.years.ago, gender: 0, organisational_unit: ou[:abteilung])
 Member.create!(first_name: 'Sonja', last_name: 'Janzek', scout_name: 'Shyra', email: 'shyra@pfadismn.ch', birthdate: 22.years.ago, gender: 0)
 Member.create!(first_name: 'Sabine', last_name: 'Schibli', scout_name: 'Coco', email: 'coco@pfadismn.ch', birthdate: 24.years.ago, gender: 0)
 Member.create!(first_name: 'Dominik', last_name: 'Näpfli', scout_name: 'Pippin', email: 'pippin@pfadismn.ch', birthdate: 19.years.ago, gender: 0)
