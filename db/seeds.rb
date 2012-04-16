8ef10fc93997b5929d6a2449d153f90c07d000ab
Diego Steiner <diego.steiner@renuo.ch>
2012-04-17 00:16:28 +0200

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,8 +25,8 @@ Member.create!(first_name: 'Sabine', last_name: 'Schibli', scout_name: 'Coco', e
 Member.create!(first_name: 'Dominik', last_name: 'Näpfli', scout_name: 'Pippin', email: 'pippin@pfadismn.ch', birthdate: 19.years.ago, gender: 0)
 Member.create!(first_name: 'Eva', last_name: 'Müller', scout_name: 'Chaja', email: 'chaja@pfadismn.ch', birthdate: 19.years.ago, gender: 0)
 
-User.create!(email: 'filou@pfadismn.ch', password: '1234', password_confirmation: '1234', roles: [:admin], member: filou)
-User.create!(email: 'gulli@pfadismn.ch', password: 'defaultSMNpw', password_confirmation: 'defaultSMNpw', roles: [:admin], member: gulli)
+User.create!(email: 'filou@pfadismn.ch', password: '1234', password_confirmation: '1234', roles: [:admin], member: filou, as: :admin)
+User.create!(email: 'gulli@pfadismn.ch', password: 'defaultSMNpw', password_confirmation: 'defaultSMNpw', roles: [:admin], member: gulli, as: :admin)
 filou.reload
 gulli.reload
 
