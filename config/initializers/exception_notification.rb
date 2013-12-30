4c52187c7f1eda727a9b216498f32cb12dc0e48a
Diego P. Steiner <diego.steiner@u041.ch>
2013-12-30 14:07:27 +0100

    exception notification fixed

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,8 @@
 if Rails.env.production?
-  Rails.application.config.middleware.use ExceptionNotifier,
-    :email_prefix => "Exception Notification Pfadismn ",
-    :sender_address => %{"Pfadismn Exception Notifier" <exception_notifier@pfadismn.ch>},
-    :exception_recipients => %w{diego.steiner@u041.ch andre.buerkler@u041.ch}
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+    email: {
+        email_prefix: "[Exception] ",
+        sender_address: %{"Pfadismn Exception Notifier" <exception_notifier@pfadismn.ch>},
+        exception_recipients: %w{diego.steiner@u041.ch andre.buerkler@u041.ch}
+    }
 end
