4c963d0cdf660355b82e151664ee2269c3c0a061
Diego P. Steiner <diego.steiner@renuo.ch>
2014-11-11 22:55:54 +0100

    exceptionnotification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,4 +48,12 @@ Pfadismn::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  Pfadismn::Application.config.middleware.use ExceptionNotification::Rack,
+    email: {
+        email_prefix: "[Exception] ",
+        sender_address: %{"Pfadismn Exception Notifier" <exception_notifier@pfadismn.ch>},
+        exception_recipients: %w{diego.steiner@u041.ch andre.buerkler@u041.ch}
+    }
+
 end
--- config/initializers/exception_notification.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env.production?
-  Rails.application.config.middleware.use ExceptionNotification::Rack,
-    email: {
-        email_prefix: "[Exception] ",
-        sender_address: %{"Pfadismn Exception Notifier" <exception_notifier@pfadismn.ch>},
-        exception_recipients: %w{diego.steiner@u041.ch andre.buerkler@u041.ch}
-    }
-end
