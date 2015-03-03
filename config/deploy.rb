cf1075baaff75c21226873f0d7114ff1e7236d29
Diego P. Steiner <diego.steiner@renuo.ch>
2015-03-03 22:17:34 +0100

    deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :app_path, lambda { "#{deploy_to}/#{current_path}" }
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/email.yml', 'config/application.yml', 'log', 'public/photos', 'var']
+set :shared_paths, ['config/database.yml', 'config/email.yml', 'config/application.yml', 'log', 'public/photos', 'var', '.env']
 
 
 # This task is the environment that is loaded for most commands, such as
