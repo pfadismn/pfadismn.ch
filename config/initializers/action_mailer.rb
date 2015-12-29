d79c13267fb8ae50c9c9d2f6d2cd1bb27ada6090
Diego P Steiner <diego.steiner@renuo.ch>
2015-12-29 11:52:48 +0100

    mailer configuration
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -3,25 +3,20 @@
 
 ActionMailer::Base.default_options = {
   from: ENV['MAIL_SENDER'],
-  reply_to: ENV['MAIL_SENDER'],
+  reply_to: ENV['MAIL_SENDER']
 }
-ActionMailer::Base.default_url_options = {host: ENV['APP_HOST'], port: ENV['APP_PORT']}
+ActionMailer::Base.default_url_options = { host: ENV['APP_HOST'], port: ENV['APP_PORT'] }
 
-if Rails.env.production?
-  filename = File.join(File.dirname(__FILE__), '..', 'email.yml')
-  if File.file?(filename)
-    mailconfig = YAML::load_file(filename)
-
-    if mailconfig.is_a?(Hash) && mailconfig.has_key?(Rails.env)
-      # Enable deliveries
-      ActionMailer::Base.perform_deliveries = true
-
-      mailconfig[Rails.env].each do |k, v|
-        v.symbolize_keys! if v.respond_to?(:symbolize_keys!)
-        ActionMailer::Base.send("#{k}=", v)
-      end
-    end
-  end
+if ENV['MAIL_USERNAME'].present? && ENV['MAIL_PASSWORD'].present?
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['MAIL_USERNAME'],
+    password: ENV['MAIL_PASSWORD'],
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    enable_starttls_auto: true,
+    authentication: :plain,
+    domain: ENV['APP_HOST']
+  }
 elsif Rails.env.development?
   ActionMailer::Base.delivery_method = :letter_opener
 end
